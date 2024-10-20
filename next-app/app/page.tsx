7f27f64fa3b3f41e10a90ea37a1c169f2e28e0e8
githubotoro <yupuday.git@gmail.com>
2024-10-20 08:12:11 -0700

    feat(market): bg update

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -2,7 +2,7 @@ import { TabSelector } from "./_components";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center place-content-start min-h-screen p-5 pb-5 gap-5 pt-20 font-[family-name:var(--font-geist-sans)] bg-gray-200">
+    <div className="flex flex-col items-center place-content-start min-h-screen p-5 pb-5 gap-5 pt-20 font-[family-name:var(--font-geist-sans)] bg-white">
       <main className="flex flex-col gap-8 row-start-2 items-center place-content-start">
         <img src="/logo.png" className="w-60 h-28 object-cover mb-2" />
 
