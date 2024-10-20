52bd52fb4557b0b8f02d020f27578ada2a22775f
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2024-10-20 00:38:08 -0700

    fix ipfs length

--- lit-action/browser/src/litAction.js
+++ lit-action/browser/src/litAction.js
@@ -1,5 +1,4 @@
-export const litActionCode = `
-(async () => {
+const litActionCode = async () => {
   // Updated fetch request with x-www-form-urlencoded body
   const tweetUrl = "https://bam-backend-five.vercel.app/api/checkTweet";
   const tweetResponse = await fetch(tweetUrl, {
@@ -8,27 +7,35 @@ export const litActionCode = `
       "Content-Type": "application/x-www-form-urlencoded"
     },
     body: new URLSearchParams({
-      tweetid: tweetId
+      tweetid: "1847143637345530235"
     })
   }).then(response => response.json());
 
-  if (tweetResponse.followerCount < followerCountThreshold) {
-    return;
-  }
+  console.log(tweetResponse)
 
-  const abi = [
-    "function checkTweet(string tweetId) public view returns (bool)"
-  ];
+  const rpcUrl = await Lit.Actions.getRpcUrl({ chain: "sepolia" });
+  const contractAddress = "0xcFF39356EDe9145F9b7DDbCCf43957BF057627a3"
 
-  // Convert an empty string to bytes and hash it
-  let toSign = ethers.utils.arrayify(
-    ethers.utils.keccak256(ethers.utils.toUtf8Bytes(tweetId))
-  );
+  ethers.utils.AbiCoder.decode(["uint256", "string"], data);
 
-  // Sign the empty message
-  const sigShare = await LitActions.signEcdsa({ toSign, publicKey, sigName });
+  // if (tweetResponse.followerCount < followerCountThreshold) {
+  //   return;
+  // }
 
-  // Publish or handle the signature as needed
-  console.log(sigShare);
-})();
-`;
+  // const abi = [
+  //   "function checkTweet(string tweetId) public view returns (bool)"
+  // ];
+
+  // // Convert an empty string to bytes and hash it
+  // let toSign = ethers.utils.arrayify(
+  //   ethers.utils.keccak256(ethers.utils.toUtf8Bytes(tweetId))
+  // );
+
+  // // Sign the empty message
+  // const sigShare = await LitActions.signEcdsa({ toSign, publicKey, sigName });
+
+  // // Publish or handle the signature as needed
+  // console.log(sigShare);
+};
+
+litActionCode();
